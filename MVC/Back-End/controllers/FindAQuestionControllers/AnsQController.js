180904d97f907bac9ecefd292713c87f4eb5783a
nox491 <57879123+nox491@users.noreply.github.com>
2021-07-29 21:10:40 +0300

    Add files via upload

---
+++ MVC/Back-End/controllers/FindAQuestionControllers/AnsQController.js
@@ -0,0 +1,23 @@
+const db = require("../../models");
+const Answers = db.answers;
+
+exports.addAnswer = (req, res) => {
+
+    //Create Registry
+    const answer = {
+        email: req.body.email,
+        title: req.body.title,
+        content: req.body.content
+    };
+    //Save Answer in db
+    Answers.create(answer)
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: "Some error occurred while adding the answer"
+        });
+    });
+
+}
